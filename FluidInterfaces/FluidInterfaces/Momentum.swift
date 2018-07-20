95930dad0e70c36d411be427b5e014185299e422
Nathan Gitter <nathan@Nathans-MacBook-Pro-3.local>
2018-07-19 21:59:22 -0500

    basic momentum view setup

--- FluidInterfaces/FluidInterfaces/Momentum.swift
+++ FluidInterfaces/FluidInterfaces/Momentum.swift
@@ -7,9 +7,101 @@
 //
 
 import UIKit
+import UIKit.UIGestureRecognizerSubclass
 
 class MomentumInterfaceViewController: InterfaceViewController {
     
-    //
+    private lazy var momentumView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = UIColor(white: 0.3, alpha: 1)
+        return view
+    }()
+    
+    private let panRecognier = InstantPanGestureRecognizer()
+    private var animator = UIViewPropertyAnimator()
+    
+    private var isOpen = false
+    private var animationProgress: CGFloat = 0
+    
+    private var closedTransform = CGAffineTransform.identity
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.addSubview(momentumView)
+        momentumView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 8).isActive = true
+        momentumView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -8).isActive = true
+        momentumView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        momentumView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 80).isActive = true
+        
+        closedTransform = CGAffineTransform(translationX: 0, y: view.bounds.height * 0.6)
+        momentumView.transform = closedTransform
+        
+        panRecognier.addTarget(self, action: #selector(panned))
+        momentumView.addGestureRecognizer(panRecognier)
+        
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        let maskLayer = CAShapeLayer()
+        maskLayer.path = UIBezierPath(roundedRect: momentumView.bounds, byRoundingCorners: [.topLeft, .topRight], cornerRadii: CGSize(width: 20, height: 20)).cgPath
+        momentumView.layer.mask = maskLayer
+        
+    }
+    
+    @objc private func panned(recognizer: UIPanGestureRecognizer) {
+        switch recognizer.state {
+        case .began:
+            startAnimationIfNeeded()
+            animator.pauseAnimation()
+            animationProgress = animator.fractionComplete
+        case .changed:
+            var fraction = -recognizer.translation(in: momentumView).y / closedTransform.ty
+            if isOpen { fraction *= -1 }
+            if animator.isReversed { fraction *= -1 }
+            animator.fractionComplete = fraction + animationProgress
+        case .ended, .cancelled:
+            let yVelocity = recognizer.velocity(in: momentumView).y
+            let shouldClose = yVelocity > 0
+            if yVelocity == 0 {
+                animator.continueAnimation(withTimingParameters: nil, durationFactor: 0)
+                break
+            }
+            if isOpen {
+                if !shouldClose && !animator.isReversed { animator.isReversed.toggle() }
+                if shouldClose && animator.isReversed { animator.isReversed.toggle() }
+            } else {
+                if shouldClose && !animator.isReversed { animator.isReversed.toggle() }
+                if !shouldClose && animator.isReversed { animator.isReversed.toggle() }
+            }
+            animator.continueAnimation(withTimingParameters: nil, durationFactor: 0)
+        default: break
+        }
+    }
+    
+    private func startAnimationIfNeeded() {
+        if animator.isRunning { return }
+        let timingParameters = UISpringTimingParameters(damping: 1, response: 0.4)
+        animator = UIViewPropertyAnimator(duration: 0, timingParameters: timingParameters)
+        animator.addAnimations {
+            self.momentumView.transform = self.isOpen ? self.closedTransform : .identity
+        }
+        animator.addCompletion { position in
+            if position == .end { self.isOpen.toggle() }
+        }
+        animator.startAnimation()
+    }
+    
+}
+
+class InstantPanGestureRecognizer: UIPanGestureRecognizer {
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent) {
+        super.touchesBegan(touches, with: event)
+        self.state = .began
+    }
     
 }
