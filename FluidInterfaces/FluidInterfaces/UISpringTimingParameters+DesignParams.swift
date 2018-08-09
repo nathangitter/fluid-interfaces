f5760eac1713826bb97e71e88dd455bfafcbdf1c
Ralf Ebert <info@ralfebert.de>
2018-08-09 11:37:47 +0200

    Extract UISpringTimingParameters+DesignParams.swift

---
+++ FluidInterfaces/FluidInterfaces/UISpringTimingParameters+DesignParams.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+extension UISpringTimingParameters {
+
+    /// A design-friendly way to create a spring timing curve.
+    ///
+    /// - Parameters:
+    ///   - damping: The 'bounciness' of the animation. Value must be between 0 and 1.
+    ///   - response: The 'speed' of the animation.
+    ///   - initialVelocity: The vector describing the starting motion of the property. Optional, default is `.zero`.
+    public convenience init(damping: CGFloat, response: CGFloat, initialVelocity: CGVector = .zero) {
+        let stiffness = pow(2 * .pi / response, 2)
+        let damp = 4 * .pi * damping / response
+        self.init(mass: 1, stiffness: stiffness, damping: damp, initialVelocity: initialVelocity)
+    }
+
+}
